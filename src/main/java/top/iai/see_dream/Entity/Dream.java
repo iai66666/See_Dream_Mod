d90ccb7316814fea4a3b635cd6eda5095575fc1c
Kinkiri <@unllcom>
2025-01-25 20:47:27 +0800

    增加了方块同类变化

--- src/main/java/top/iai/see_dream/Entity/Dream.java
+++ src/main/java/top/iai/see_dream/Entity/Dream.java
@@ -10,32 +10,58 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 //不在视野中的方块会随机变化
 public class Dream extends Entity
 {
     private final Random random = new Random();
-    //计时用的
     private long lastExecutionTime = 0;
-
     public Dream(World worldIn)
     {
         super(worldIn);
     }
+    //各种表
+    private static final List<Block> logsBlocks = Arrays.asList(Blocks.LOG, Blocks.LOG2);
+    private static final List<Block> leavesBlocks = Arrays.asList(Blocks.LEAVES, Blocks.LEAVES2);
+    private static final List<Block> dirtBlocks = Arrays.asList(Blocks.DIRT, Blocks.GRASS, Blocks.GRASS_PATH, Blocks.SAND, Blocks.FARMLAND);
+    private static final List<Block> stonesBlocks = Arrays.asList(Blocks.STONE, Blocks.COAL_ORE, Blocks.IRON_ORE, Blocks.GOLD_ORE, Blocks.DIAMOND_ORE);
+    private static final List<Block> airBlocks = Arrays.asList(Blocks.ICE, Blocks.GLASS, Blocks.TNT);
+    //表的表
+    private static final List<List<Block>> blocksListsList = Arrays.asList(logsBlocks, leavesBlocks, dirtBlocks, stonesBlocks);
+    //字典
+    private static final Map<Block, List<Block>> blockTransformMap = new HashMap<>();
 
-    //可能变成的方块列表
-    private static final List<Block> possibleBlocks = new ArrayList<>();
-    static
-    {
-        //在这里添加可以变成的方块
-        possibleBlocks.add(Blocks.ICE);//我超，冰！
-        possibleBlocks.add(Blocks.LOG);//橡木原来叫log
-        possibleBlocks.add(Blocks.STONE);
+    static {
+        //神秘增强for循环（写字典）
+        for (List<Block> blocksList : blocksListsList) {
+            for (Block block : blocksList) {
+                blockTransformMap.put(block, blocksList);
+            }
+        }
     }
+    /*初版：看着就吓人
+    static {
+        blockTransformMap.put(Blocks.LOG, Arrays.asList(Blocks.LOG, Blocks.LOG2));
+        blockTransformMap.put(Blocks.LOG2, Arrays.asList(Blocks.LOG, Blocks.LOG2));
 
+        blockTransformMap.put(Blocks.LEAVES, Arrays.asList(Blocks.LEAVES, Blocks.LEAVES2));
+        blockTransformMap.put(Blocks.LEAVES2, Arrays.asList(Blocks.LEAVES, Blocks.LEAVES2));
+
+        blockTransformMap.put(Blocks.DIRT, Arrays.asList(Blocks.DIRT, Blocks.GRASS, Blocks.GRASS_PATH, Blocks.SAND, Blocks.FARMLAND));
+        blockTransformMap.put(Blocks.GRASS, Arrays.asList(Blocks.DIRT, Blocks.GRASS, Blocks.GRASS_PATH, Blocks.SAND, Blocks.FARMLAND));
+        blockTransformMap.put(Blocks.GRASS_PATH, Arrays.asList(Blocks.DIRT, Blocks.GRASS, Blocks.GRASS_PATH, Blocks.SAND, Blocks.FARMLAND));
+        blockTransformMap.put(Blocks.SAND, Arrays.asList(Blocks.DIRT, Blocks.GRASS, Blocks.GRASS_PATH, Blocks.SAND, Blocks.FARMLAND));
+        blockTransformMap.put(Blocks.FARMLAND, Arrays.asList(Blocks.DIRT, Blocks.GRASS, Blocks.GRASS_PATH, Blocks.SAND, Blocks.FARMLAND));
+
+        blockTransformMap.put(Blocks.STONE, Arrays.asList(Blocks.STONE, Blocks.COAL_ORE, Blocks.IRON_ORE, Blocks.GOLD_ORE, Blocks.DIAMOND_ORE));
+        blockTransformMap.put(Blocks.COAL_ORE, Arrays.asList(Blocks.STONE, Blocks.COAL_ORE, Blocks.IRON_ORE, Blocks.GOLD_ORE, Blocks.DIAMOND_ORE));
+        blockTransformMap.put(Blocks.IRON_ORE, Arrays.asList(Blocks.STONE, Blocks.COAL_ORE, Blocks.IRON_ORE, Blocks.GOLD_ORE, Blocks.DIAMOND_ORE));
+        blockTransformMap.put(Blocks.GOLD_ORE, Arrays.asList(Blocks.STONE, Blocks.COAL_ORE, Blocks.IRON_ORE, Blocks.GOLD_ORE, Blocks.DIAMOND_ORE));
+        blockTransformMap.put(Blocks.DIAMOND_ORE, Arrays.asList(Blocks.STONE, Blocks.COAL_ORE, Blocks.IRON_ORE, Blocks.GOLD_ORE, Blocks.DIAMOND_ORE));
+
+        airBlocks.addAll(Arrays.asList(Blocks.ICE, Blocks.DIRT, Blocks.LOG, Blocks.STONE));
+    }*/
     @Override
     public void onUpdate()
     {if (world != null) {
@@ -55,6 +81,7 @@ public class Dream extends Entity
         double posZ = 0;
         float yaw = 0;
         float pitch = 0;
+
         if (world != null) {
         World world = Minecraft.getMinecraft().world;
             //偷iai的代码 iai:草
@@ -70,28 +97,46 @@ public class Dream extends Entity
                     } else return;
                 }
             }
-            //定义变化范围，范围半径为128长、128宽、32高
+            //定义变化范围，范围半径为128长、128宽、8高
             int rangeX = 128;
-            int rangeY = 32;
+            int rangeY = 8;
             int rangeZ = 128;
+
             //在范围内取随机数
             int randomX = (int)posX - rangeX + random.nextInt(2 * rangeX + 1);
-            int randomY = (int)posY - rangeY + random.nextInt(2 * rangeY + 1);
+            int randomY = (int)posY - rangeY + random.nextInt(2 * rangeY + 1);//这里让头顶距离高一点
             int randomZ = (int)posZ - rangeZ + random.nextInt(2 * rangeZ + 1);
-            BlockPos newBlockPos = new BlockPos(randomX, randomY, randomZ);
-            //距离判断是好使的，这个圆锥判断有点问题
+
+            //要替换的方块坐标
+            BlockPos blockPos = new BlockPos(randomX, randomY, randomZ);
+
+            //计算玩家视线方向
             double var3 = Math.cos(-yaw * 0.017453292 - (float)Math.PI);
             double var5 = Math.sin(-yaw * 0.017453292 - (float)Math.PI);
             double var7 = -Math.cos(-pitch * 0.017453292);
             double var9 = Math.sin(-pitch * 0.017453292);
             Vec3d VEC = new Vec3d(var5 * var7, var9, var3 * var7);
-            System.out.println("yaw"  + " " + yaw + " " + "pitch" + pitch + " " + VEC.x + " " + VEC.y + " " + VEC.z);
+
+            //视锥判断+距离判断
             if (!isPointInCone(randomX, randomY, randomZ, posX, posY, posZ, VEC.x,  VEC.y,  VEC.z, 65) && Math.sqrt(Math.pow(randomX - posX, 2) + Math.pow(randomY - posY, 2) + Math.pow(randomZ - posZ, 2)) > 20) {
-                //随机选择新的方块
-                Block newBlock = possibleBlocks.get(random.nextInt(possibleBlocks.size()));
+                //获取要替换的方块
+                Block currentBlock = world.getBlockState(blockPos).getBlock();
+                //声明新方块
+                Block newBlock;
+                //尝试获取该方块的变化表
+                List<Block> possibleTransformations = blockTransformMap.get(currentBlock);
+                //判断是不是特殊方块
+                if (possibleTransformations != null && !possibleTransformations.isEmpty()) {
+                    // 从对应的表中随机选择一个方块
+                    newBlock = possibleTransformations.get(random.nextInt(possibleTransformations.size()));
+                }
+                //啥也不是，从空气表中选
+                else {
+                    newBlock = airBlocks.get(random.nextInt(airBlocks.size()));
+                }
                 //替换方块
                 synchronized(newBlock){
-                    world.setBlockState(newBlockPos, newBlock.getDefaultState(), 3);
+                    world.setBlockState(blockPos, newBlock.getDefaultState(), 3);
                 }
             }
             /*
@@ -103,7 +148,7 @@ public class Dream extends Entity
             int endY = (int) posY + 32;
             int endZ = (int) posZ + 128;
             // 遍历该范围内的方块,这部分循环逻辑只有天顶星电脑才能挺住，**建议重写**
-            // 确实是一坨，注释保留当笑话看：）
+            // 确实是一坨，注释保留当笑话看：）神秘for循坏
             for (int x = startX; x <= endX; x++) {
                 for (int z = startZ; z <= endZ; z++) {
                     for (int y = startY; y <= endY; y++) {
