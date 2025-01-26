332836ea3620853db1aafe35ea52b83ea263f60b
iai66666 <ls584320@163.com>
2025-01-26 13:34:38 +0800

    注册了每tick运行事件

--- src/main/java/top/iai/see_dream/See_Dream.java
+++ src/main/java/top/iai/see_dream/See_Dream.java
@@ -1,6 +1,8 @@
 package top.iai.see_dream;
 
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
+import top.iai.see_dream.dream.dream;
 
 // 注解Mod，声明模组信息
 @Mod(modid = See_Dream.MODID, name = See_Dream.NAME, version = See_Dream.VERSION, useMetadata = true)
@@ -8,4 +10,8 @@ public class See_Dream {
     public static final String MODID = "see_dream";
     public static final String NAME = "梦现";
     public static final String VERSION = "0.0.1";
+    public See_Dream() {
+    MinecraftForge.EVENT_BUS.register(new dream());
+    }
 }
+
